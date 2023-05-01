527f42e63b2de25804b777752286d3e4c4a3e1bd
Lukasnvc <lukasnvc@gmail.com>
2023-05-01 15:48:19 +0300

    have to find bug in itemCard

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { Text, View } from "react-native";
 
 import Discover from "./screens/Discover";
 import HomeScreen from "./screens/HomeScreen";
+import ItemScreen from "./screens/ItemScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { TailwindProvider } from "tailwindcss-react-native";
@@ -17,6 +19,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["tailwindcss-react-native/babel"],
-    [['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true
-    }]]
+    plugins: [
+      "tailwindcss-react-native/babel",
+      ["@babel/plugin-transform-modules-commonjs", { allowTopLevelThis: true }],
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
